b9b6faaa3cc8370a9d23a2391847b2d1800fe0bc
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-26 16:53:50 +0530

    create home page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,69 @@
-
+import React from "react";
+import {Box, Button, HStack, Heading, Image, Stack, Text, VStack} from '@chakra-ui/react'
+import './Home.css'
+import { Link } from "react-router-dom";
+import vg from '../../assets/images/bg.png'
+import {CgGoogle,CgYoutube } from "react-icons/cg";
+import {SiCoursera,SiUdemy} from 'react-icons/si'
+import{DiAws} from 'react-icons/di'
+import introVideo from '../../assets/videos/intro.mp4'
 
 //define home function 
 function Home(){
-    return(
-        <h1>home</h1>
-    );
+    return <section className="home">
+      <div className="container">
+            <Stack 
+               direction={["column","row"]}
+               height='100%'
+               justifyContent={["center","space-between"]}
+               alignItems="center"
+               spacing={['16','56']}
+
+            >
+                <VStack width={"full"} alignItems={["center","flex-end"]}>
+                    <Heading children="LEARN FROM THE EXPERTS" size={'2x1'}/>
+                    <Text children="Find Valuable Content at Reasonable Price"/>
+                    <Link to='/courses'>
+                        <Button size={'lg'} colorScheme="yellow">
+                            Explore Now
+                        </Button>
+                    </Link>
+                </VStack>
+                 <Image
+                  className="vector-graphics"
+                   boxSize={"md"} 
+                   src={vg} 
+                   objectFit='contain' 
+                  />
+            </Stack>
+      </div>
+
+         <Box padding={'8'} bg="blackAlpha.800">
+            <Heading
+               textAlign={'center'}
+               fontFamily="body"
+               color={'yellow.400'}
+               children="OUR BRANDS"
+            />
+            <HStack className="brandsBanner" justifyContent={"space-evenly"} marginTop="4">
+               <CgGoogle/>
+               <CgYoutube/>
+               <SiCoursera/>
+               <SiUdemy/>
+               <DiAws/> 
+            </HStack>
+         </Box>
+
+         <div className="container2">
+            <video autoPlay
+            controls 
+            controlsList="nodownload nofullscreen noremoteplayback" 
+            disablePictureInPicture
+            disableRemotePlayback
+            src={introVideo}
+            ></video>
+         </div>
+    </section>
 }
 
 //export 
