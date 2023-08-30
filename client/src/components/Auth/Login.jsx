9aaa28f7b8e35e7f85ee5ec8c00313ff8d387463
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-30 12:20:42 +0530

    setup for login page

---
+++ client/src/components/Auth/Login.jsx
@@ -0,0 +1,10 @@
+import { Box, Button, Container, FormLabel, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+
+  return (<>Login</>)
+}
+
+export default Login
