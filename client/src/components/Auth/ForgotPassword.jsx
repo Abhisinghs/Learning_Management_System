9736774cc8b39403ada1e08bac3661e93ac15603
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-30 16:37:56 +0530

    complete forgot password page

--- client/src/components/Auth/ForgotPassword.jsx
+++ client/src/components/Auth/ForgotPassword.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const ForgotPassword = () => {
-  return (
-    <div>ForgotPassword</div>
-  )
+
+    const [email,setEmail] = useState("")
+  return <Container py={"16"} height={"90vh"}>
+
+    <form>
+
+        <Heading
+          children="Forgot Password "
+          my="16"
+          textTransform={"uppercase"}
+          textAlign={["center","left"]}
+        />
+
+        <VStack spacing="8">
+            <Input 
+                required 
+                id='email' 
+                value={email} 
+                onChange={ e=> setEmail(e.target.value)}
+                placeholder='abc@gmail.com'
+                type={'email'}
+                focusBorderColor='blue.500'
+            />
+            
+            <Button 
+                type='submit'
+                w={"full"} 
+                colorScheme='blue'
+            >
+                Send Reset Link
+            </Button>
+        </VStack>
+    </form>
+  </Container>
 }
 
 export default ForgotPassword
