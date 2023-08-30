b058cafaaeadec427d307ff9eeb9d1fa239bfaef
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-30 12:41:44 +0530

    setup for register page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Layout/Header/Header';
 import Courses from './components/Courses/Courses';
 import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/courses' element={<Courses />}/>
         <Route path='/login' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
       </Routes>
 
       <Footer/>
