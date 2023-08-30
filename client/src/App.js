5785a34239b985a0c1ef350ed2d051a5e95679e6
Abhisinghs <abhisheksingh35716@gmail.com>
2023-08-30 16:47:18 +0530

    setup the reset password page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Footer from './components/Layout/Footer/Footer';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import ForgotPassword from './components/Auth/ForgotPassword';
+import ResetPassword from './components/Auth/ResetPassword';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
         <Route path='/forgotpassword' element={<ForgotPassword />}/>
+        <Route path='/resetpassword/:token' element={<ResetPassword />}/>
       </Routes>
 
       <Footer/>
