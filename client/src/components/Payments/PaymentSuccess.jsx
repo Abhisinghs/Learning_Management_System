a74d7149793a44f866eeacdf4650cc8132013216
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-08 18:01:32 +0530

    working on payment success page

--- client/src/components/Payments/PaymentSuccess.jsx
+++ client/src/components/Payments/PaymentSuccess.jsx
@@ -1,10 +1,45 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { RiCheckboxCircleFill } from 'react-icons/ri'
 
 const PaymentSuccess = () => {
   return <Container h="90vh" p="16">
-    <Heading children="You have Pro Pack"/>
-    <Heading>You have Pro Pack</Heading>
+    <Heading my="8" textAlign={'center'} children="You have Pro Pack"/>
+    <VStack
+      boxShadow={'lg'}
+      pb="16"
+      alignItems={'center'}
+      borderRadius="lg"
+    >
+      <Box 
+        w='full'
+        bg="blue.400"
+        p="4"
+        css={{borderRadius:'8px 8px 0 0'}}
+      >
+        <Text
+          children="Payment Success"
+        />
+      </Box> 
+
+      <Box p="4">
+        <VStack
+          textAlign={'center'}
+          px="8"
+          mt="4"
+          spacing={'8'}
+        >
+        <Text 
+          children="Congratulation you're a pro member.
+          You have access to premium content"
+        />
+
+        <Heading size={'4xl'}>
+          <RiCheckboxCircleFill/>
+        </Heading>
+        </VStack>
+      </Box>
+    </VStack>
   </Container>
 }
 
