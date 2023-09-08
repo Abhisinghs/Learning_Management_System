18c5a3d3772a2998efdeb4f4e759e75f4dff86fe
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-08 17:51:10 +0530

    subscribe page complete

--- client/src/components/Payments/PaymentSuccess.jsx
+++ client/src/components/Payments/PaymentSuccess.jsx
@@ -1,9 +1,11 @@
+import { Container, Heading } from '@chakra-ui/react'
 import React from 'react'
 
 const PaymentSuccess = () => {
-  return (
-    <div>PaymentSuccess</div>
-  )
+  return <Container h="90vh" p="16">
+    <Heading children="You have Pro Pack"/>
+    <Heading>You have Pro Pack</Heading>
+  </Container>
 }
 
 export default PaymentSuccess
--- client/src/components/Payments/Subscribe.jsx
+++ client/src/components/Payments/Subscribe.jsx
@@ -1,8 +1,8 @@
-import { Box, Container, Heading, Text, VStack } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const Subscribe = () => {
-  return <Container h='90vh' p='16'>
+  return <Container h='90vh' p='10'>
     <Heading
       children='Welcome' 
       my="8"
@@ -24,10 +24,30 @@ const Subscribe = () => {
       <Box p='4'>
         <VStack textAlign={'center'} px="8" mt={'4'} spacing="8">
           <Text 
-            color={'black'} 
             children={'Join pro pack and get access to all content.'}
           />
+
+          <Heading size="md" children={'$50 only'}/>
         </VStack>
+
+        <Button my="8" w="full" colorScheme={'blue'}>
+          Buy Now
+        </Button>
+      </Box>
+
+      <Box 
+        bg={'blackAlpha.600'}
+        p="4"
+        css={{borderRadius:'0 0 8px 8px'}}
+      >
+        <Heading  
+          color={'white'}
+          textTransform='uppercase'
+          size='sm'
+          children={'100% refund at cancellation'}
+        />
+
+        <Text fontSize={'xs'} color="white" children="Terms & Conditions Apply"/>
       </Box>
     </VStack>
   </Container>
