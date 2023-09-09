868ed454ff0c6653d6f6af5711a5b4c70a248969
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-09 18:07:36 +0530

    completed profile page

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
-import { Avatar, Button, Container, HStack, Heading, Stack, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import { Avatar, Button, Container, HStack, Heading, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, Text, VStack, useDisclosure } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { RiDeleteBin7Fill } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
+import { fileUploadCss } from '../Auth/Register'
 
 const Profile = () => {
 
@@ -15,10 +17,21 @@ const Profile = () => {
         playlist:[
             {
                 course:'ahdfkdf',
-                poster:'sdhfkdjfk'
+                poster:'https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453_640.jpg'
             }
         ]
+    };
+
+    const removeFromPlaylistHandler = (id) =>{
+        console.log(id);
     }
+
+    const changeImageSubmitHandler = (e,image)=>{
+        e.preventDefault();
+        console.log(image);
+    };
+
+    const {isOpen,onClose,onOpen} = useDisclosure()
   return <Container minH={'95vh'} maxW="container.lg" py="8">
     <Heading
         children="Profile"
@@ -36,7 +49,7 @@ const Profile = () => {
 
     <VStack>
         <Avatar boxSizing={'48'}/>
-        <Button colorScheme={'blue'} variant={'ghost'}>
+        <Button onClick={onOpen} colorScheme={'blue'} variant={'ghost'}>
             Change Photo
         </Button>
     </VStack>
@@ -83,7 +96,88 @@ const Profile = () => {
     </Stack>
 
     <Heading children='Playlist' size={'md'} my='8' />
+
+    {
+        user.playlist.length >0 && (
+            <Stack
+              direction={['column','row']}
+              alignItems={'center'}
+              flexWrap="wrap"
+              p="4"
+            >
+
+              {
+                user.playlist.map((element)=>(
+                    <VStack w="48" m="2" key={element.course}>
+                      <Image
+                        boxSize={'full'}
+                        objectFit='contain'
+                        src={element.poster}
+                      />
+
+                      <HStack>
+                        <Link to={`/course/${element.course}`}>
+                            <Button variant={'ghost'} colorScheme='blue'> Watch Now</Button>
+                        </Link>
+
+                        <Button onClick={()=> removeFromPlaylistHandler(element.course)}>
+                            <RiDeleteBin7Fill/>
+                        </Button>
+                      </HStack>
+                    </VStack>
+                ))
+              }
+            </Stack>
+        )
+    }
+
+    <ChangePhotoBox changeImageSubmitHandler={changeImageSubmitHandler} isOpen={isOpen} onClose={onClose}/>
   </Container>
 }
 
-export default Profile
+export default Profile;
+
+function ChangePhotoBox({isOpen,onClose,changeImageSubmitHandler}){
+
+    const [image,setImage] = useState('');
+    const [imagePrev,setImagePrev]= useState('')
+    const changeImage =e =>{
+        const file=e.target.files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+
+        reader.onloadend =()=>{
+            setImagePrev(reader.result);
+            setImage(file);
+        }
+    }
+    const closeHandler = ()=>{
+        onClose();
+        setImage("");
+        setImagePrev("");
+    }
+    return (
+        <Modal isOpen={isOpen} onClose={closeHandler}>
+            <ModalOverlay backdropFilter={'blue(10px)'} />
+            <ModalContent>
+                <ModalHeader children="Change Photo" />
+                <ModalCloseButton/>
+                <ModalBody>
+                    <Container>
+                        <form onSubmit={(e)=> changeImageSubmitHandler(e,image)}>
+                            <VStack spacing={'8'}>
+                                {imagePrev && <Avatar src={imagePrev} boxSize={'48'}/>}
+                                <Input type={'file'}  css={{'&::file-selector-button':fileUploadCss} } onChange={changeImage} />
+                                <Button w='full' colorScheme={'blue'} type='submit' >Change</Button>
+                            </VStack>
+                        </form>
+                    </Container>
+                </ModalBody>
+                <ModalFooter>
+                    <Button mr='3' onClick={closeHandler}>Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
