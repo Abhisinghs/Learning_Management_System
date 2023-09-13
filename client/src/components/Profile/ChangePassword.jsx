0c7b84f8eeac94c2b589eae6ed6f966a337134ab
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-13 23:32:55 +0530

    complete change password page

--- client/src/components/Profile/ChangePassword.jsx
+++ client/src/components/Profile/ChangePassword.jsx
@@ -6,7 +6,36 @@ const ChangePassword = () => {
   const [oldPassword,setOldPassword] = useState("");
   const [newPassword,setNewPassword] = useState("");
   return <Container py='16' minH={'90vh'}>
-    
+    <form>
+      <Heading
+        textTransform={'uppercase'}
+        children="Change Password"
+        my="16"
+        textAlign={['center','left']}
+      />
+
+      <VStack>
+        <Input 
+          required 
+          value={oldPassword} 
+          onChange={ e=> setOldPassword(e.target.value)}
+          placeholder='Old Password '
+          type={'password'}
+          focusBorderColor='blue.500'
+        />
+        
+        <Input 
+          required 
+          value={newPassword} 
+          onChange={ e=> setNewPassword(e.target.value)}
+          placeholder='New Password '
+          type={'password'}
+          focusBorderColor='blue.500'
+        />
+
+        <Button w='full' colorScheme={'blue'} type='submit'>Change</Button>
+      </VStack>
+    </form>
   </Container>
 }
 
