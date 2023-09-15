20f231e8a5e424f70f1cebf83c2ac73adc12e844
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-15 11:51:27 +0530

    working on update profile page

--- client/src/components/Profile/UpdateProfile.jsx
+++ client/src/components/Profile/UpdateProfile.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const UpdateProfile = () => {
-  return (
-    <div>UpdateProfile</div>
-  )
+
+  const [oldPassword,setOldPassword] = useState("");
+  const [newPassword,setNewPassword] = useState("");
+  return <Container py='16' minH={'90vh'}>
+    <form>
+      <Heading
+        textTransform={'uppercase'}
+        children="Update Profile"
+        my="16"
+        textAlign={['center','left']}
+      />
+
+      <VStack>
+        <Input 
+          required 
+          value={name} 
+          onChange={ e=> setName(e.target.value)}
+          placeholder='Name '
+          type={'text'}
+          focusBorderColor='blue.500'
+        />
+        
+        <Input 
+          required 
+          value={email} 
+          onChange={ e=> setEmail(e.target.value)}
+          placeholder='Email'
+          type={'email'}
+          focusBorderColor='blue.500'
+        />
+
+        <Button w='full' colorScheme={'blue'} type='submit'>Change</Button>
+      </VStack>
+    </form>
+  </Container>
 }
 
-export default UpdateProfile
+export default UpdateProfile;
