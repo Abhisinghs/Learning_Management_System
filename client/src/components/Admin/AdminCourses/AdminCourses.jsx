e9bc7d229b13d4789436d5c2353a18eb61c38be6
Abhisinghs <abhisheksingh35716@gmail.com>
2023-09-17 19:50:26 +0530

    working on Course Modal page

--- client/src/components/Admin/AdminCourses/AdminCourses.jsx
+++ client/src/components/Admin/AdminCourses/AdminCourses.jsx
@@ -1,104 +1,151 @@
-import { Box, Button, Grid, HStack, Heading, Table, TableCaption, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
-import React from 'react'
-import cursor from '../../../assets/images/cursor.png'
-import Sidebar from '../Sidebar'
-import { RiDeleteBin7Fill } from 'react-icons/ri'
+import {
+  Box,
+  Button,
+  Grid,
+  HStack,
+  Heading,
+  Image,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from '@chakra-ui/react';
+import React from 'react';
+import cursor from '../../../assets/images/cursor.png';
+import Sidebar from '../Sidebar';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import CourseModal from './CourseModal';
 
 const AdminCourses = () => {
-
-  const users=[{
-    _id:"adfdskfjdksfdks",
-    name:'abhishek',
-    role:"admin",
-    subscription:{
-      status:"active"
+  const courses = [
+    {
+      _id: 'adfdskfjdksfdks',
+      title: 'React course',
+      category: 'web development',
+      poster: {
+        url: 'https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_960_720.jpg',
+      },
+      createdBy: 'Learn Hub',
+      views: 123,
+      numOfVideos: 12,
     },
-    email:"abhi@gmail.com"
-  }];
+  ];
 
-  const updateHandler = (userId)=>{
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const courseDetailsHandler = userId => {
     console.log(userId);
-  }
+  };
 
-  const deleteButtonHandler=(userId)=>{
+  const deleteButtonHandler = userId => {
     console.log(userId);
+  };
+
+  const deleteLectureButtonHandler = (courseId, lectureId) => {
+    console.log(courseId);
+    console.log(lectureId);
+  };
+
+  const addLectureHandler =(e,courseId,title,description,video)=>{
+    e.preventDefault();
   }
+  return (
+    <Grid
+      css={{
+        cursor: `url(${cursor}),default`,
+      }}
+      minH={'100vh'}
+      templateColumns={['1fr', '5fr 1fr']}
+    >
+      <Box p={['0', '8']} overflowX={'auto'}>
+        <Heading
+          textTransform={'uppercase'}
+          children="All Users"
+          my="16"
+          textAlign={['center', 'left']}
+        />
 
-  return <Grid
-    css={{
-        cursor:`url(${cursor}),default`,
-    }}
-    minH={'100vh'}
-    templateColumns={['1fr','5fr 1fr']}
-  >
-    <Box p={['0','16']} overflowX={'auto'} >
-       <Heading
-         textTransform={'uppercase'}
-         children="All Users"
-         my='16'
-         textAlign={['center','left']}
-       />
+        <TableContainer w={['100vw', 'full']}>
+          <Table variant={'simple'} size={'lg'}>
+            <TableCaption>All avialable courses in the database</TableCaption>
+            <Thead>
+              <Tr>
+                <Th>Id</Th>
+                <Th>Poster</Th>
+                <Th>Title</Th>
+                <Th>Category</Th>
+                <Th>Creator</Th>
+                <Th isNumeric>Views</Th>
+                <Th isNumeric>Lectures</Th>
+                <Th isNumeric>Action</Th>
+              </Tr>
+            </Thead>
 
-       <TableContainer w={['100vw','full']}>
-        <Table variant={'simple'} size={'lg'}>
-           <TableCaption>All avialable users in the database</TableCaption>
-           <Thead>
-             <Tr>
-               <Th>Id</Th>
-               <Th>Name</Th>
-               <Th>Email</Th>
-               <Th>Role</Th>
-               <Th>Subscription</Th>
-               <Th isNumeric>Action</Th>
-             </Tr>
-           </Thead>
+            <Tbody>
+              {courses.map(item => (
+                <Row
+                  courseDetailsHandler={courseDetailsHandler}
+                  deleteButtonHandler={deleteButtonHandler}
+                  key={item._id}
+                  item={item}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
 
-           <Tbody>
-             {
-                users.map(item=>(
-                  <Row updateHandler={updateHandler} deleteButtonHandler={deleteButtonHandler} key={item._id} item={item}/>
-                ))
-             }
-           </Tbody>
-        </Table>
-       </TableContainer>
-    </Box>
+        <CourseModal
+          isOpen={isOpen}
+          id={'dkfjdkfjdkfjd'}
+          courseTitle="React course"
+          onClose={onClose}
+          deleteLectureButtonHandler={deleteButtonHandler}
+          addLectureHandler={addLectureHandler}
+        />
+      </Box>
 
-    <Sidebar/>
-  </Grid>
-}
+      <Sidebar />
+    </Grid>
+  );
+};
 
+export default AdminCourses;
 
-export default AdminCourses
-
-function Row({item,updateHandler,deleteButtonHandler}){
-  return(
+function Row({ item, courseDetailsHandler, deleteButtonHandler }) {
+  return (
     <Tr>
       <Td>#{item._id}</Td>
-      <Td>{item.name}</Td>
-      <Td>{item.email}</Td>
-      <Td>{item.role}</Td>
-      <Td>{item.subscription.status === 'active' ?"Active" :"Not Active"}</Td>
+      <Td>
+        <Image src={item.poster.url} />
+      </Td>
+      <Td>{item.title}</Td>
+      <Td textTransform={'uppercase'}>{item.category}</Td>
+      <Td>{item.createdBy}</Td>
+      <Td isNumeric>{item.views}</Td>
+      <Td isNumeric>{item.numOfVideos}</Td>
+
       <Td isNumeric>
         <HStack justifyContent={'flex-end'}>
-          <Button 
-            onClick={()=> 
-            updateHandler(item._id)} 
-            variant={'outline'} 
+          <Button
+            onClick={() => courseDetailsHandler(item._id)}
+            variant={'outline'}
             color="purple.500"
           >
-            Change Role
+            View Lectures
           </Button>
 
-
-          <Button 
-            color={'purple.600'} 
-            onClick={()=>deleteButtonHandler(item._id)}
+          <Button
+            color={'purple.600'}
+            onClick={() => deleteButtonHandler(item._id)}
           >
-            <RiDeleteBin7Fill/>
+            <RiDeleteBin7Fill />
           </Button>
-        </HStack>  
+        </HStack>
       </Td>
     </Tr>
-  )
+  );
 }
