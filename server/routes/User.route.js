8a387b7bded123ba7b6783b46d43d443b0321ede
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-02 14:09:12 +0530

    working on forgetpassword logic

--- server/routes/User.route.js
+++ server/routes/User.route.js
@@ -1,38 +1,46 @@
-import express from 'express'
-import {register,login, logout, getMyProfile, changePassword, updateProfile, updateProfilePicture, forgetPassword, resetPassword } from '../controllers/User.controller.js';
-import isAuthenticated from '../middlewares/Auth.js';
-
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  getMyProfile,
+  changePassword,
+  updateProfile,
+  updateProfilePicture,
+  forgetPassword,
+  resetPassword,
+} from "../controllers/User.controller.js";
+import isAuthenticated from "../middlewares/Auth.js";
 
 const router = express.Router();
 
-
-//To register a new user 
-router.route('/register').post(register);
+//To register a new user
+router.route("/register").post(register);
 
 //Login
-router.route('/login').post(login)
+router.route("/login").post(login);
 
-//Logout 
-router.route('/logout').get(logout);
-
+//Logout
+router.route("/logout").get(logout);
 
 //get my profile
-router.route('/me').get(isAuthenticated, getMyProfile);
+router.route("/me").get(isAuthenticated, getMyProfile);
 
 //change password
-router.route('/changepassword').put(isAuthenticated, changePassword);
+router.route("/changepassword").put(isAuthenticated, changePassword);
 
 //update profile
-router.route('/updateprofile').put(isAuthenticated,updateProfile);
+router.route("/updateprofile").put(isAuthenticated, updateProfile);
 
 //update profile picture
-router.route('/updateprofilepicture').put(isAuthenticated,updateProfilePicture);
+router
+  .route("/updateprofilepicture")
+  .put(isAuthenticated, updateProfilePicture);
 
 //forgot password
-router.route('/forgetpassword').post(forgetPassword);
+router.route("/forgetpassword").post(forgetPassword);
 
 //reset password
-router.route('/resetpassword').post(resetPassword);
-
+router.route("/resetpassword/token:").post(resetPassword);
 
 export default router;
