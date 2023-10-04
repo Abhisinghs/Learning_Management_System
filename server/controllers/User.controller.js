cfa5a59957d8593ea13912ba6cbe9b33288ad073
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-04 23:46:24 +0530

    test executed successfully

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -156,7 +156,7 @@ const forgetPassword = catchAsynError(async(req,res,next)=>{
 
   const resetToken= await user.getResetToken();
 
-   const url=`${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
+  const url=`${process.env.FRONTEND_URL}/resetpassword/${resetToken}`
    
   const message = `Click on the link to reset your password. ${url}. If you have not request then please ignore.`
   //send token via email 
