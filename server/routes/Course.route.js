494f5e06a4e3d094b5f656e1937eb4e7f3c8fbb0
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-10 23:18:33 +0530

    api tested successfully

--- server/routes/Course.route.js
+++ server/routes/Course.route.js
@@ -9,7 +9,7 @@ router.route('/courses').get(getAllCourses);
 //create a new courses only admin
 router.route('/createcourses').post(createCourse);
 
-//Add lecture 
+//Add lecture  //get lectures  
 router.route('/course/:id').get(getCourseLectures); 
 
 //Delete lecture,get course details
