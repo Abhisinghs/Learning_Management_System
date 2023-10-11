310bb908248f2242522c4c13c091489a3ad557b9
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-11 23:26:39 +0530

    working on multer middleware

--- server/routes/Course.route.js
+++ server/routes/Course.route.js
@@ -1,21 +1,24 @@
-import express from 'express'
-import { createCourse, getAllCourses, getCourseLectures } from '../controllers/Course.controller.js';
+import express from "express";
+import {
+  addLecture,
+  createCourse,
+  getAllCourses,
+  getCourseLectures,
+} from "../controllers/Course.controller.js";
 
 const router = express.Router();
 
 //Get All courses without lectures
-router.route('/courses').get(getAllCourses);
+router.route("/courses").get(getAllCourses);
 
 //create a new courses only admin
-router.route('/createcourses').post(createCourse);
+router.route("/createcourses").post(createCourse);
 
-//Add lecture  //get lectures  
-router.route('/course/:id').get(getCourseLectures).post(
-    
-); 
+//Add lecture  //get lectures
+router.route("/course/:id").get(getCourseLectures).post(addLecture);
 
 //Delete lecture,get course details
 
-//delete lecture, 
+//delete lecture,
 
-export default router
+export default router;
