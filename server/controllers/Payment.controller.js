116552b554a9b01d479654a7dd18fa13db4342a2
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-27 23:25:02 +0530

    working on paymentVerification Api

--- server/controllers/Payment.controller.js
+++ server/controllers/Payment.controller.js
@@ -30,23 +30,14 @@ const buySubscription = catchAsynError(async (req,resp,next)=>{
 
 
 const paymentVerification = catchAsynError(async (req,resp,next)=>{
+
+    const {razorpay_signature,razorpay_payment_id,razorpay_subscription_id}= req.body;
+
     const user = await User.findById(req.user._id);
 
-    if(user.role==="admin")
-      return next(new ErrorHandler("Admin Can't buy Subscription",400));
+    const subscription_id= user.subscription.id;
 
-    const plan_id= process.env.PLAIN_ID;
 
-    const subscription= await instance.subscriptions.create({
-        plan_id: plan_id,
-        customer_notify: 1,
-        total_count: 12,
-    })
-
-    user.subscription.id= subscription.id;
-    user.subscription.status= subscription.status;
-
-    await user.save();
 
     resp.status(201).json({
         success:true,
