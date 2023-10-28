19e9febdf23394a591762a9738a5aec6e3d44dc5
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-28 15:37:40 +0530

    payment modal completed successfully

--- server/models/Payment.modal.js
+++ server/models/Payment.modal.js
@@ -1,7 +1,23 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-   
+    razorpay_payment_id:{
+        type:String,
+        required:true,
+    },
+    razorpay_signature:{
+        type:String,
+        required:true,
+    },
+    razorpay_subscription_id:{
+        type:String,
+        required:true,
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
 })
 
 const Payment = mongoose.model("Payments",paymentSchema);
