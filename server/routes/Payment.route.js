e9b7ed88eb9e3e7d801f9db0cd8245d6e1ccaa29
Abhisinghs <abhisheksingh35716@gmail.com>
2023-10-30 22:21:26 +0530

    working on cancel subscription logic

--- server/routes/Payment.route.js
+++ server/routes/Payment.route.js
@@ -13,4 +13,7 @@ router.route('/paymentverification').post(isAuthenticated,paymentVerification);
 //Get Razorpay key
 router.route('/razorpaykey').get(getRazorpayKey);
 
+//Cancel Subscription 
+router.route('/subscribe/cancel').delete(isAuthenticated,);
+
 export default router;
