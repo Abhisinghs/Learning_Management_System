a81266572e4f2be42f6957a9dfef4122acec1153
Abhisinghs <abhisheksingh35716@gmail.com>
2023-11-02 12:46:07 +0530

    test api on postman

--- server/controllers/Other.controller.js
+++ server/controllers/Other.controller.js
@@ -1,13 +1,34 @@
-import catchAsynError from "../middlewares/catchAsynError";
+import catchAsynError from "../middlewares/catchAsynError.js";
+import sendEmail from "../utils/sendEmail.js";
 
 const contact= catchAsynError(async(req,resp,next)=>{
+
+    const {name,email,message}=req.body;
+    const to=process.env.MY_MAIL;
+
+    const subject="Contact From LearnHub";
+    const text=`I am ${name} and my Email is ${email}. \n ${message}`;
+
+    await sendEmail(to,subject,text);
+
     resp.status(200).json({
-        success:true
+        success:true,
+        message:"Your Message has been sent."
     })
 })
+
 const courseRequest= catchAsynError(async(req,resp,next)=>{
+    const {name,email,course}=req.body;
+    const to=process.env.MY_MAIL;
+
+    const subject="Request for a course on LearnHub";
+    const text=`I am ${name} and my Email is ${email}. \n ${course}`;
+
+    await sendEmail(to,subject,text);
+
     resp.status(200).json({
-        success:true
+        success:true,
+        message:"Your Request has been sent."
     })
 })
 
