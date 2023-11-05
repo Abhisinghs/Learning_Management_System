dd8b3dd736e220446e895d998ee6a5d3c7d4c766
Abhisinghs <abhisheksingh35716@gmail.com>
2023-11-05 18:00:33 +0530

    working on stats modal

---
+++ server/models/Stats.modal.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema({
+    
+    users:{
+        type:Number,
+        default:0
+    },
+    subscriptions:{
+        type:Number,
+        default:0
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+const stats = mongoose.model("stats",statsSchema);
+
+//export modal
+export default stats;
