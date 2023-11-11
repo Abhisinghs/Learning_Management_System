d8972e7177a56f3b9ad6d0a6c0a3a966dbb9ee39
Abhisinghs <abhisheksingh35716@gmail.com>
2023-11-11 09:52:14 +0530

    add regex functionality in api

--- server/controllers/Course.controller.js
+++ server/controllers/Course.controller.js
@@ -11,7 +11,20 @@ const getAllCourses = catchAsynError(async function getAllCourses(
   resp,
   next
 ) {
-  const courses = await Course.find().select("-lectures");
+
+  const keyword = req.query.keyword || "";
+  const category = req.query.category || "";
+
+  const courses = await Course.find({
+    title:{
+      $regex:keyword,
+      $options:"i",
+    },
+    category:{
+      $regex:category,
+      $options:"i",
+    }
+  }).select("-lectures");
   resp.status(200).json({
     success: true,
     courses,
